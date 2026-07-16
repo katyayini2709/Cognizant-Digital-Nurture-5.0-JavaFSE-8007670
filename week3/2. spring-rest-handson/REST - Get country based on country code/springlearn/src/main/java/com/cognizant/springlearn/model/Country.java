79dd671b94db08cc9ad54aa118dc3f57005f55c0
Katyayini Badda <baddakatyayini@gmail.com>
2026-07-16 20:13:41 +0530

    done

---
+++ week3/2. spring-rest-handson/REST - Get country based on country code/springlearn/src/main/java/com/cognizant/springlearn/model/Country.java
@@ -0,0 +1,45 @@
+package com.cognizant.springlearn.model;
+
+public class Country {
+
+    private String code;
+    private String name;
+
+    // Default Constructor
+    public Country() {
+    }
+
+    // Parameterized Constructor
+    public Country(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    // Getter for code
+    public String getCode() {
+        return code;
+    }
+
+    // Setter for code
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
